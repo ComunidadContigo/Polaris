912cf4f76e156cb7131df1559488e718aa46d432
Danyalfaro <daniel.alfaro@upr.edu>
2021-10-14 14:00:18 -0400

    Added mapview and draggable markers.

--- screens/MapScreen/MapScreen.tsx
+++ screens/MapScreen/MapScreen.tsx
@@ -1,19 +1,62 @@
-import React, { useContext } from 'react';
-import { View, Text, Button } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { AuthContext } from '../../components/context';
+import React, { useState } from 'react';
+import { View, Dimensions, StyleSheet, Text } from 'react-native';
+import MapView, { Callout, Marker } from 'react-native-maps';
 
 const MapScreen = () => {
-  const { signOut } = useContext(AuthContext);
+  const [mapRegion, setMapRegion] = useState(null);
+  const [currentLocation, setCurrentLocation] = useState({
+    latitude: 18.21194,
+    longitude: -67.14225,
+  });
+  const [meetingLocation, setMeetingLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+  const [destination, setDestination] = useState(null);
+
+  console.log('CL: ', currentLocation);
+  console.log('ML: ', meetingLocation);
   return (
     <View
       style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
     >
-      <Text>Map</Text>
-      <Button title="Sign Out" onPress={() => signOut()} />
-      <StatusBar />
+      <MapView
+        style={styles.map}
+        provider="google"
+        initialRegion={{
+          ...currentLocation,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={currentLocation}
+          pinColor="green"
+          draggable
+          onDragStart={(e) => {
+            console.log('Drag started. ', e.nativeEvent.coordinate);
+          }}
+          onDragEnd={(e) => {
+            setMeetingLocation({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            });
+          }}
+        >
+          <Callout>
+            <Text>Im Here!</Text>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
 
 export default MapScreen;
