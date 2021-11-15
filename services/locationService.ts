3384ebd3f6d8ec8bfcf2c4d7fb5331ed9e772d57
jerrybassat7 <jerry.bassat@upr.edu>
2021-11-14 21:16:55 -0500

    Add geolocation (#47)
    * Adding Expo-Location, attempting to print location,  GetcurrentpositionAsync() not working
    
    * Working
    
    * Work
    
    * Adding Location to Map Screen
    
    * Addding Display for Coords in mapviewq
    
    * Working on Sirius Fetch Implementation
    
    * Working, Sends Location to Database when location changes while in mapscreen
    
    * Merging with Dev
    
    * using combine coordinates function on body
    
    * Update screens/MapScreen/MapScreen.tsx
    
    Co-authored-by: Daniel Alfaro R. <33332094+danyalfaro@users.noreply.github.com>
    
    * Update screens/MapScreen/MapScreen.tsx
    
    Co-authored-by: Daniel Alfaro R. <33332094+danyalfaro@users.noreply.github.com>
    
    * Update screens/MapScreen/MapScreen.tsx
    
    Co-authored-by: jerrybassat7 <jerry.bassat@upr.com>
    Co-authored-by: Daniel Alfaro R. <33332094+danyalfaro@users.noreply.github.com>

---
+++ services/locationService.ts
@@ -0,0 +1,28 @@
+import * as expoLocation from 'expo-location';
+import { Location } from '../models/Location';
+
+export const getlocation = async () => {
+  const { status } =
+    await expoLocation.requestForegroundPermissionsAsync();
+  // const prov = expoLocation.getProviderStatusAsync();
+  // console.log(prov);
+  if (status !== 'granted') {
+    console.log('Permission not granted');
+  }
+  // console.log(status);
+  const isLocationServicesEnabled =
+    await expoLocation.hasServicesEnabledAsync();
+  // console.log(isLocationServicesEnabled);
+
+  console.log('Getting Location');
+  const userlocation = await expoLocation.getCurrentPositionAsync({});
+  console.log(userlocation.coords.latitude);
+  console.log(userlocation.coords.longitude);
+  const lat = userlocation.coords.latitude;
+  const lon = userlocation.coords.longitude;
+  const location: Location = {
+    coordinates: { latitude: lat, longitude: lon },
+    description: 'Latest Location',
+  };
+  return location;
+};
