2b6f5a3a1657c068131bad893279c4981e07da30
Danyalfaro <daniel.alfaro@upr.edu>
2021-11-19 18:00:21 -0400

    Added getter functions for user info.

--- services/User/index.ts
+++ services/User/index.ts
@@ -1,5 +1,46 @@
 import { UserInfo } from '../../models/user.model';
+import envs from '../../config/environment';
+import { siriusFetch } from '../httpService';
 
-// export const getUserFromRid = (rid: number): UserInfo => {
+export const getUserFromRid = async (
+  accessToken: string | undefined,
+  setAccessToken: any,
+  uid: number,
+  rid: number,
+): Promise<UserInfo | undefined> => {
+  // https://sirius-ingress-gabgc.cloud.okteto.net/user/requester/1
+  const endpoint = `${envs?.DEV_USER_SERVICE_URL}/requester/${rid}`;
+  try {
+    const res = await siriusFetch(
+      accessToken,
+      setAccessToken,
+      uid,
+      endpoint,
+    );
+    console.log(res);
+  } catch (e) {
+    console.log(e);
+  }
+  return undefined;
+};
 
-// }
+export const getUserFromBid = async (
+  accessToken: string | undefined,
+  setAccessToken: any,
+  uid: number,
+  bid: number,
+): Promise<UserInfo | undefined> => {
+  const endpoint = `${envs?.DEV_USER_SERVICE_URL}/buddy/${bid}`;
+  try {
+    const res = await siriusFetch(
+      accessToken,
+      setAccessToken,
+      uid,
+      endpoint,
+    );
+    console.log(res);
+  } catch (e) {
+    console.log(e);
+  }
+  return undefined;
+};
